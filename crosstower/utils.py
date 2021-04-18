c62de7727cd7d5e97e310505019b3411b0e1ee17
Cooper Lewis <thatcooperlewis@gmail.com>
2021-04-17 21:01:44 -0400

    move shared functions to utils file

---
+++ crosstower/utils.py
@@ -0,0 +1,17 @@
+from requests import Response
+from crosstower.config import API_BASE_URL
+
+
+def build_url(endpoint: str) -> str:
+    if endpoint[0] == '/':
+        endpoint = endpoint[1:]
+    return f"{API_BASE_URL}/{endpoint}"
+
+
+def handle_response(response: Response, request_name: str = 'API') -> dict:
+    # TODO - Make this an entire class to parse error types
+    if response.status_code != 200:
+        error_str = f'Unexpected {request_name} response: {response.status_code}'
+        print(response.content)
+        raise Exception(error_str)
+    return response.json()
