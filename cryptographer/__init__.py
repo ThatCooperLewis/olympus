592bc27e29749951756b4ec5656685edf281648e
Cooper Lewis <thatcooperlewis@gmail.com>
2021-06-27 00:57:05 -0400

    add zeus to init file

--- cryptographer/__init__.py
+++ cryptographer/__init__.py
@@ -2,3 +2,4 @@ from cryptographer.athena import Athena
 from cryptographer.delphi import Delphi, ConfidenceTicker
 from cryptographer.hermes import Hermes
 from cryptographer.prometheus import Prometheus
+from cryptographer.zeus import Zeus
