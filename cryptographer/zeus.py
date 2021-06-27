8bcab5c503a242f1effbc9686d3df0c5c1032495
Cooper Lewis <thatcooperlewis@gmail.com>
2021-06-27 00:56:10 -0400

    Create empty zeus file

---
+++ cryptographer/zeus.py
@@ -0,0 +1,14 @@
+# TODO
+'''
+
+1. Init new, slow-pace Athena scraper for most recent prices
+2. Init Hermes, get queue
+3. Init Delphi, pass queue from Hermes
+4. Listen for errors
+
+'''
+
+class Zeus:
+
+    def __init__(self) -> None:
+        pass
