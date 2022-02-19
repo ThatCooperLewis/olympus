cb4e050741f433f4852a40789992b656cf130108
Cooper Lewis <thatcooperlewis@gmail.com>
2022-02-18 16:27:54 -0800

    Update discord alert with credential parser

--- utils/discord.py
+++ utils/discord.py
@@ -1,9 +1,14 @@
 import requests
+import json
 from utils import Logger 
+
+
+# TODO: 
 
 class DiscordWebhook:
 
-    def __init__(self, webhook_url):
+    def __init__(self, credentials_file: str):
+        webhook_url = self.get_webhook_from_json_file(credentials_file)
         self.url = webhook_url
         self.log = Logger.setup(self.__class__.__name__)
 
@@ -11,3 +16,8 @@ class DiscordWebhook:
             payload = {"username": username, "content": message}
             self.log.debug(f'Sending discord alert: {message}')
             _ = requests.post(self.url, data=payload)
+
+    def get_webhook_from_json_file(self, file_path: str):
+        with open(file_path) as json_file:
+            data: dict = json.load(json_file)
+            return data.get("discord_webhook")
