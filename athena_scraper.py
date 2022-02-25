3dc6470a6274f480e2dfeb82f30cc18aba44389f
Cooper Lewis <thatcooperlewis@gmail.com>
2022-02-24 19:08:12 -0800

    athena_scraper discord announcement

--- athena_scraper.py
+++ athena_scraper.py
@@ -1,13 +1,15 @@
-from utils import Logger
+from utils import Logger, DiscordWebhook
 from olympus.athena import Athena
 
 class AthenaScraper:
 
     def __init__(self) -> None:
         self.log = Logger.setup(self.__class__.__name__)
+        self.discord = DiscordWebhook(self.__class__.__name__)
         self.athena = Athena(custom_interval=60)
 
     def run(self) -> None:
+        self.discord.send_alert("AthenaScraper has started a new run.")
         self.athena.run(headless=True)
 
 if __name__ == '__main__':
