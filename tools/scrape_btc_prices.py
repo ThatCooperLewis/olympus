61b5c38578de268f299e20573b8dca2e73975bc0
Cooper Lewis <thatcooperlewis@gmail.com>
2022-02-24 19:06:14 -0800

    delete old file

--- tools/scrape_btc_prices.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
-from olympus import Athena
-
-if __name__ == "__main__":
-    csv_path = sys.argv[1]
-    Athena().run(csv_path)
