e1416651505de4f1cd444b5150b9b07915839502
Cooper Lewis <thatcooperlewis@gmail.com>
2022-02-27 00:55:40 -0800

    add UUID to PredictionVector

--- olympus/utils/helper_objects.py
+++ olympus/utils/helper_objects.py
@@ -1,13 +1,15 @@
 from queue import Queue
 from utils import Logger
+import uuid
 class PredictionVector:
 
     # Simple solution for useful queue objects
     # Composes the Hermes order queue
     def __init__(self, weight, predictions, timestamp):
-        self.weight = weight
-        self.prediction_history = predictions
-        self.timestamp = timestamp
+        self.weight: float = weight
+        self.prediction_history: list = predictions
+        self.timestamp: int = timestamp
+        self.uuid: str = uuid.uuid4().hex
 
 
 class PredictionQueue:
