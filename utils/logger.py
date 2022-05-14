58181c2e317a6f64d0ec6bd9c30efcb63e5063bc
Cooper Lewis <thatcooperlewis@gmail.com>
2022-05-14 04:08:37 -0700

    Fix duplicate logger bug

--- utils/logger.py
+++ utils/logger.py
@@ -17,9 +17,11 @@ class Logger():
         :type category_str: str
         :return: A logger object.
         '''
-        logger = cls.__get_existing_logger_if_exists(category_str)
-
-        # TODO: Check for existing log object for category_str, use that instead
+        loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+        for logger in loggers:
+            if logger.name == category_str:
+                return logger
+        logger = logging.getLogger(category_str)
         
 
         # Setup CLI logging stream
@@ -42,11 +44,3 @@ class Logger():
     @classmethod
     def git_hash(cls):
         return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
-
-    @classmethod
-    def __get_existing_logger_if_exists(self, name):
-        loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
-        for logger in loggers:
-            if logger.name == name:
-                return logger
-        return logging.getLogger(name)
